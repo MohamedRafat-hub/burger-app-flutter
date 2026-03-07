ed0a3f0cdd4720cbeeed2975cf38accba6a7381b
mohamedrafat <mtag66870@gmail.com>
2026-03-07 14:48:59 +0200

    add app icon and name and create native splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,12 +2,9 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_app/core/constants/app_colors.dart';
 import 'package:food_app/core/network/api_service.dart';
-import 'package:food_app/features/auth/data/managers/profile_cubit/profile_cubit.dart';
-
 import 'package:food_app/features/auth/data/repos/auth_repo.dart';
 import 'package:food_app/features/auth/presentation/views/login_view.dart';
 import 'package:food_app/root.dart';
@@ -23,16 +20,20 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _fadeAnimation;
-  late Animation<double> _scaleAnimation;
-  late Animation<Offset> _slideAnimation;
+
+  late Animation<Offset> _topImageAnimation;
+  late Animation<Offset> _bottomImageAnimation;
 
   AuthRepo authRepo = AuthRepo(apiService: ApiService());
+
+  bool _isChecking = false;
 
   Future<void> checkLogin() async {
     try {
       final user = await authRepo.autoLogin();
+
       if (!mounted) return;
+
       if (user != null) {
         log('User exist');
       } else {
@@ -40,36 +41,30 @@ class _SplashViewState extends State<SplashView>
       }
 
       if (user != null) {
-        debugPrint('continue as a user');
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return Root();
-        }));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Root()),
+        );
       } else if (AuthRepo.isGuest) {
-        debugPrint('continue as a guest');
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return Root();
-        }));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Root()),
+        );
       } else {
-        debugPrint('go to login');
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return LoginView();
-        }));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginView()),
+        );
       }
     } catch (e) {
-      debugPrint('catch go to login');
       if (mounted) {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) {
-          return LoginView();
-        }));
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginView()),
+        );
       }
     }
   }
-
-  bool _isChecking = false;
 
   @override
   void initState() {
@@ -77,35 +72,37 @@ class _SplashViewState extends State<SplashView>
 
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 3),
     );
 
-    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeIn,
-    ));
+    _topImageAnimation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutBack,
+      ),
+    );
 
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeOutBack,
-    ));
-
-    _slideAnimation =
-        Tween<Offset>(begin: const Offset(0, 0.5), end: Offset.zero)
-            .animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeOut,
-    ));
+    _bottomImageAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutBack,
+      ),
+    );
 
     _controller.forward();
 
     if (!_isChecking) {
       _isChecking = true;
       Future.delayed(
-          Duration(seconds: 3),
+        const Duration(seconds: 3),
         checkLogin,
       );
-
     }
   }
 
@@ -119,28 +116,32 @@ class _SplashViewState extends State<SplashView>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: Center(
-        child: Column(
-          children: [
-            const Gap(200),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-            /// Logo Animation
-            FadeTransition(
-              opacity: _fadeAnimation,
-              child: ScaleTransition(
-                scale: _scaleAnimation,
-                child: SvgPicture.asset('assets/images/logo.svg'),
+              SlideTransition(
+                position: _topImageAnimation,
+                child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                  height: 120,
+                ),
               ),
-            ),
 
-            const Spacer(),
+              const Gap(20),
 
-            /// Bottom Image Animation
-            SlideTransition(
-              position: _slideAnimation,
-              child: Image.asset('assets/images/splash.png'),
-            ),
-          ],
+              SlideTransition(
+                position: _bottomImageAnimation,
+                child: Image.asset(
+                  'assets/images/splash.png',
+                  height: 220,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
